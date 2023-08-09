5c116d54af11e4ea24759c386b928fdec43e11b4
Mohamed Gamal <94301975+Jimmy6754@users.noreply.github.com>
2023-08-09 21:22:11 +0300

    Add files via upload

---
+++ screens/CityDetailScreen.tsx
@@ -0,0 +1,170 @@
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
+import { useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+import WeatherDetail from "../Components/WeatherDetail";
+
+const Width = Dimensions.get("window").width;
+
+const CityDetailScreen = ({ route }: any) => {
+  const navigation = useNavigation();
+  const headerName = route.params.name;
+  const headerCountry = route.params.country;
+
+  // Get request from the api
+  let [isLoading, setLoading] = useState<boolean>(true);
+  // let [error, setError]: any | null = useState();
+  let [response, setResponse]: any | null = useState();
+
+  const apiKey: string = "f5cb0b965ea1564c50c6f1b74534d823";
+  useLayoutEffect(() => {
+    if (response && response?.name === headerName) {
+      return;
+    }
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        headerName +
+        "&appid=" +
+        apiKey
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setResponse(result);
+          setLoading(false);
+          return result;
+        },
+        (error) => {
+          setLoading(false);
+          // setError(error);
+        }
+      );
+  }, [setLoading, setResponse]);
+  // Get request from the api
+
+  // loading Indicator
+  const loadingHandler = () => {
+    if (isLoading === true) {
+      return <ActivityIndicator size="large" />;
+    }
+    return (
+      <>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={{
+              uri:
+                "http://openweathermap.org/img/w/" +
+                response?.weather[0].icon +
+                ".png",
+            }}
+            style={style.weatherIcon}
+          />
+        </View>
+        <WeatherDetail
+          description={response?.weather[0].description}
+          temperature={Math.trunc(response?.main.temp - 273.15)}
+          humidity={response?.main.humidity}
+          windspeed={Math.trunc(response?.wind.speed * (18 / 5))}
+        />
+      </>
+    );
+  };
+  // loading Indicator
+
+  const navigateBackHandler = () => {
+    navigation.navigate("citySelector");
+  };
+
+  return (
+    <View>
+      <View style={style.blueHeadercontainer}>
+        <Pressable onPress={navigateBackHandler}>
+          <Image
+            source={require("../assets/arrow_back-24px.png")}
+            style={style.arrowIcon}
+          />
+        </Pressable>
+      </View>
+      <View style={style.detailscontainer}>
+        <Text style={style.cityNameHeader}>
+          {headerName + ", " + headerCountry}
+        </Text>
+        <View style={style.innerDetailscontainer}>{loadingHandler()}</View>
+      </View>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  blueHeadercontainer: {
+    width: "100%",
+    height: 150,
+    backgroundColor: "#2388C7",
+  },
+  cityMainContainer: {
+    marginTop: 10,
+  },
+  titleText: {
+    color: "#FFFFFF",
+    fontSize: 24,
+    marginTop: 99,
+    marginLeft: 72,
+  },
+  detailscontainer: {
+    position: "absolute",
+    marginTop: 105,
+    marginLeft: 32,
+    backgroundColor: "#FFFFFF",
+    width: Width - 64,
+    minWidth: 296,
+    minHeight: 423,
+    borderRadius: 4,
+    shadowColor: "black",
+  },
+  arrowIcon: {
+    marginTop: 25,
+    marginLeft: -10,
+    width: 80,
+    height: 80,
+  },
+  cityNameHeader: {
+    textAlign: "center",
+    marginTop: 26,
+    fontSize: 24,
+  },
+  weatherDetailsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "grey",
+    marginHorizontal: 32,
+    marginBottom: 7,
+  },
+  detailText: {
+    fontSize: 14,
+  },
+  numberText: {
+    fontSize: 20,
+  },
+  weatherIcon: {
+    width: 94,
+    height: 77,
+    marginTop: 68,
+    marginBottom: 76,
+  },
+  innerDetailscontainer: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    // backgroundColor: "grey",
+  },
+});
+
+export default CityDetailScreen;
---
+++ screens/CitySelectorScreen.tsx
@@ -0,0 +1,86 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { useState } from "react";
+
+import CityContainer from "../Components/CityContainer";
+import AddCityButton from "../Components/AddCityButton";
+import SearchContainer from "../Components/SearchContainer";
+
+const CitySelectorScreen = () => {
+  let [isSearch, setSearch] = useState<boolean>(false);
+  let [cityList, setCityList] = useState([{ name: "London", country: "UK" }]);
+
+  const searchModalHandler = (searchedResult: any) => {
+    const city = cityList.find((el) => {
+      return el?.name === searchedResult?.name;
+    });
+    if (!city) {
+      setCityList([searchedResult, ...cityList]);
+    }
+    setSearch(!isSearch);
+  };
+
+  const addCityHandler = () => {
+    if (!isSearch) {
+      return <AddCityButton onPress={() => setSearch(true)} />;
+    }
+    return <SearchContainer onPress={searchModalHandler} />;
+  };
+
+  return (
+    <View style={{ flex: 1, position: "relative" }}>
+      {isSearch ? (
+        <View
+          style={{
+            position: "absolute",
+            zIndex: 2,
+            backgroundColor: "rgba(0,0,0,0.5)",
+            width: "100%",
+            height: "100%",
+          }}
+        ></View>
+      ) : null}
+      <View style={style.blueHeadercontainer}>
+        <Text style={style.titleText}>Cities</Text>
+      </View>
+      <View style={style.cityMainContainer}>
+        <FlatList
+          data={cityList}
+          renderItem={(city) => {
+            return (
+              <CityContainer
+                name={city.item.name}
+                country={city.item.country}
+              />
+            );
+          }}
+        />
+      </View>
+      {addCityHandler()}
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    opacity: 0.6,
+    height: "100%",
+  },
+  blueHeadercontainer: {
+    width: "100%",
+    height: 150,
+    backgroundColor: "#2388C7",
+  },
+  cityMainContainer: {
+    marginTop: 10,
+  },
+  titleText: {
+    color: "#FFFFFF",
+    fontSize: 24,
+    marginTop: 99,
+    marginLeft: 72,
+  },
+});
+
+export default CitySelectorScreen;
